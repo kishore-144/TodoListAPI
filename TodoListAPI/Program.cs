26e9baecdffd370a4e1e3633eb716852cdb6368f
Kishore B <142733678+kishore-144@users.noreply.github.com>
2025-09-18 10:31:38 +0530

    Update Program.cs

--- TodoListAPI/Program.cs
+++ TodoListAPI/Program.cs
@@ -31,6 +31,11 @@ namespace TodoListAPI
             });
 
             var app = builder.Build();
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.Migrate(); // applies migrations if needed
+            }
             app.UseCors();
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
